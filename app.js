deb648418decd6ba7df2d82a23281303e5615d6d
daishila <rosi.bbk@gmail.com>
2022-07-21 00:52:53 -0300

    Update/Creando la repo para el proyecto api

---
+++ app.js
@@ -0,0 +1,24 @@
+const marvel = {
+    render: () => {
+      const urlAPI = 'https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=3ec36189fdc616795343136639d98b46&hash=805835ac148b073c7a8aa1e7fe18d838';
+      const container = document.querySelector('#marvel-row');
+      let contentHTML = '';
+  
+      fetch(urlAPI)
+        .then(res => res.json())
+        .then((json) => {
+          for (const hero of json.data.results) {
+            let urlHero = hero.urls[0].url;
+            contentHTML += `
+              <div class="col-md-4">
+                  <a href="${urlHero}" target="_blank">
+                    <img src="${hero.thumbnail.path}.${hero.thumbnail.extension}" alt="${hero.name}" class="img-thumbnail">
+                  </a>
+                  <h3 class="title">${hero.name}</h3>
+              </div>`;
+          }
+          container.innerHTML = contentHTML;
+        })
+    }
+  };
+  marvel.render();                
